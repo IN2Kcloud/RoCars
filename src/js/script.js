42c7de5f9befe86159935c38a83164e4ac893bea
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2025-05-02 12:25:24 -0700

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', () => {
+  document.body.classList.remove('before-load');
+});
+document.querySelector('.loading').addEventListener('transitionend', (e) => {
+  document.body.removeChild(e.currentTarget);
+});
+
 window.addEventListener("load", () => {
     setTimeout(() => {
       document.getElementById("background-video").classList.add("active");
